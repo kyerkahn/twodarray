4e07ca6c3d1e6289674fbdbafa2917d3daea98ae
Арслан Загидуллин <arslan@iMac.local>
2012-08-15 23:03:18 +0400

    Initial Commit

---
+++ TwoDArray/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TwoDArray
+//
+//  Created by Арслан Загидуллин on 15.08.12.
+//  Copyright (c) 2012 Арслан Загидуллин. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
